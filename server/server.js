307551a3365b04b21a8dcedf837b5b457c8adad2
Silent-Era <junkyard@abv.bg>
2019-06-11 01:03:14 +0300

    adding htmlplugin, executing webpack in the server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,43 @@
 const path = require('path')
 const express = require('express')
+const webpack = require('webpack')
+
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static(path.resolve(__dirname, 'dist')))
+const config = require('./webpack_configs/webpack_dev.config')
+webpack(config, (err, stats) => {
+   if (err) {
+    console.error(err.stack || err);
+    if (err.details) {
+      console.error(err.details);
+    }
+    return;
+  }
 
-app.listen(port,(err) => {
-    if(!err)
-        console.log(`brie listening on port: ${port}`)
-    else 
-        console.error(err)
+  console.log(stats.toString({colors: true}))
+
+  const info = stats.toJson();
+
+  if (stats.hasErrors()) {
+    console.error(info.errors);
+  }
+
+  if (stats.hasWarnings()) {
+    console.warn(info.warnings);
+  } 
+  
+  startServer()
 })
+
+function startServer(){
+    app.use(express.static(path.resolve(__dirname, '../dist')))
+
+    app.listen(port,(err) => {
+        if(!err)
+            console.log(`brie listening on port: ${port}`,
+            path.resolve(__dirname, '../dist'))
+        else 
+            console.error(err)
+    })
+}
--- server/webpack_configs/webpack_dev.config.js
+++ server/webpack_configs/webpack_dev.config.js
@@ -1,16 +1,22 @@
-const path = require('path');
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: '../../client/client.js',
+    entry: './client/client.js',
     output: {
-        path:path.resolve(__dirname, 'dist'),
+        path:path.resolve(__dirname, '../../dist'),
         filename:'main_bunde.js'
     },
     module: {
         rules:[
             {test:/\.jpg$/, use:'file-loader'},
             {test:/\.css$/, use: ['css-loader']},
-            {test:/\.html$/, use: {loader:'html-loader'}}
         ]
-    }
+    },
+    plugins: [
+        new HtmlPlugin({
+            title: 'Brie Bunch',
+            template: './client/index.html'
+        })
+    ]
 }
