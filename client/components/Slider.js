7d32609f5eabca82345c0ea31b60110977435d54
Silent-Era <junkyard@abv.bg>
2019-06-12 02:53:22 +0300

    adding sound to slider

--- client/components/Slider.js
+++ client/components/Slider.js
@@ -9,16 +9,49 @@ import '../media/imgs/orph.png'
 import '../media/imgs/mud.png'
 import '../media/imgs/eug.jpg'
 
+import '../media/sounds/flute.mp3'
+
 export default class Slider extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isPlaying:false
+    }
+
+    this.audioRef = React.createRef()
+
+    this.handlePlay = this.handlePlay.bind(this)
+  }
+  onPlay(){
+    console.log('player started')
+  }
+
+  handlePlay() {
+    let newState = Object.assign({}, this.state)
+    newState.isPlaying = true
+    this.setState(newState)
+
+    let audio = this.audioRef.current
+    audio.volume = 0.13
+    audio.play()
+  }
  
   render() {
     let srcString = '/imgs/'
-    console.log(peeps)
+  
     return (
+      <div>
       <div className="slider">
         {peeps.map((peep, index) => {
           return <SliderCard key={index}  src={srcString + peep.pic} name={peep.name}/>
         })}
+       
+      </div>
+      <audio ref={this.audioRef} id="sliderAudio">
+          <source src="/sounds/flute.mp3" type="audio/mpeg"/>
+      </audio>
+        <button onClick={this.handlePlay}>Play</button>
       </div>
     )
   }
--- server/webpack_configs/webpack_dev.config.js
+++ server/webpack_configs/webpack_dev.config.js
@@ -18,7 +18,14 @@ module.exports = {
             }},
             {test:/\.css$/, use: ['css-loader']},
             {test:/\.js$/, exclude:/node_modules/,loader:'babel-loader'},
-            {test:/\.styl$/, use:['style-loader','css-loader','stylus-loader']}
+            {test:/\.styl$/, use:['style-loader','css-loader','stylus-loader']},
+            {test:/\.mp3$/, use: {
+                loader:"file-loader",
+                options: {
+                    outputPath:'sounds',
+                    name:'[name].[ext]'
+                }
+            }}
         ]
     },
     plugins: [
