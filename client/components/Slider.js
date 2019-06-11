e67375bef4b56c0cf9e1af132e5a0ba241513cfe
Silent-Era <junkyard@abv.bg>
2019-06-11 20:44:20 +0300

    setting initial slider card component and further styling the slider

--- client/components/Slider.js
+++ client/components/Slider.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
 import '../styles/_slider.styl'
+import SliderCard from './SliderCards';
 
 export default class Slider extends Component {
   render() {
     return (
       <div className="slider">
-        This is the slider
+        <SliderCard/>
+        <SliderCard/>
+        <SliderCard/>
+        <SliderCard/>
       </div>
     )
   }
---
+++ client/components/SliderCards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let SliderCard = (props) => {
+  return (
+    <div className="sliderCard">
+      This is slider card
+    </div>
+  )
+}
+
+export default SliderCard
