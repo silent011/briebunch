856f14d8555ce10bf70f827ba2c128820811d768
Silent-Era <junkyard@abv.bg>
2019-06-21 06:25:34 +0300

    optimizing and achieving the wanted rotation effect of the main animation

---
+++ client/components/CircleSvg.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+import '../styles/_CircleSvg.styl'
+
+import animate from '../scripts/orbsAnimation'
+
+export default class CircleSvg extends Component {
+  componentDidMount(){
+    let stats = {}
+    animate(stats)  
+  }
+
+  render() {
+    return (
+      <div className="circle-container">
+         <svg viewBox="0 0 1400 400">
+    <g id="circle">
+      <circle id="mainCircle" cx="700" cy="200" r="120" fill="#b83dba" />
+      <circle cx="700" cy="200" r="115" fill="#ffaec8" />
+      <circle cx="700" cy="200" r="20" fill="#b83dba" />
+      <line x1="580" y1="200" x2="820" y2="200" />
+      <line x1="700" y1="80" x2="700" y2="320" />
+      <line x1="616" y1="116" x2="784" y2="284"/>
+      <line x1="784" y1="116" x2="616" y2="284"/>
+    </g>
+    <defs>
+      <pattern id="image1" height="100%" width="100%" x="0%" y="0%" viewBox="0 0 512 512">
+          <image x="0%" y="0%" width="512" height="512" xlinkHref="/imgs/eug.jpg"></image>  
+      </pattern>
+      <pattern id="image2" height="100%" width="100%" x="0%" y="0%" viewBox="0 0 512 512">
+          <image x="0%" y="0%" width="512" height="512" xlinkHref="/imgs/mud.png"></image>  
+      </pattern>
+      <pattern id="image3" height="100%" width="100%" x="0%" y="0%" viewBox="0 0 512 512">
+          <image x="0%" y="0%" width="512" height="512" xlinkHref="/imgs/orph.png"></image>  
+      </pattern>
+      <pattern id="image4" height="100%" width="100%" x="0%" y="0%" viewBox="0 0 512 512">
+          <image x="0%" y="0%" width="512" height="512" xlinkHref="/imgs/zada.jpg"></image>  
+      </pattern>
+    </defs>
+        <g id="orbs-group">
+          <circle className="orbs-clones"  cx="-48" cy="240" r="32" fill="url(#image1)" />
+          <circle className="orbs-clones" cx="-48" cy="240" r="32" fill="url(#image2)" />
+          <circle className="orbs-clones"  cx="-48" cy="240" r="32" fill="url(#image3)"/>
+          <circle className="orbs-clones"  cx="-48" cy="240" r="32" fill="url(#image4)" />
+        </g>
+        <circle className="orbs"  cx="-48" cy="240" r="32" fill="url(#image1)" />
+          <circle className="orbs" cx="-48" cy="240" r="32" fill="url(#image2)" />
+          <circle className="orbs"  cx="-48" cy="240" r="32" fill="url(#image3)"/>
+          <circle className="orbs"  cx="-48" cy="240" r="32" fill="url(#image4)" />
+   
+    </svg>
+    </div>
+    )
+  }
+}
---
+++ client/scripts/orbsAnimation.js
@@ -0,0 +1,92 @@
+import anime from 'animejs'
+
+let orbsAnimation = (orbs, orbClones) => {
+    let delay = 1250
+    let locationValues = [
+        {cx:700, cy:34},
+        {cx:534, cy: 200},
+        {cx:700, cy: 366},
+        {cx:866, cy: 200},
+
+    ]
+
+    let animateOrb = (orb, orbClone, orbNumber, newLocation) => {
+        let orbsRotation = (orbNumber) => {
+            if(orbNumber === 0) {
+                anime({
+                    targets:'#orbs-group',
+                    easing:'linear',
+                    duration:5000,
+                    loop:true,
+                    rotate:360,
+                    transformOrigin:['50% 50% 0', '50% 50% 0']
+                })
+            }          
+        }
+
+
+        let orbTravel = anime({
+            targets: orb,
+            delay: orbNumber*delay,
+            translateX: {value:748},
+            translateY: {value:-206},
+            duration:3000,
+            scale: {value:[3,1], duration:1400},
+            easing: 'linear',
+            transformOrigin:['50% 50% 0', '50% 50% 0']
+        }).finished.then(() => {
+            let {cx, cy} = newLocation
+            orb.setAttribute("style", "")
+            orbClone.setAttribute("cx", cx)
+            orbClone.setAttribute("cy", cy)
+            
+            orbsRotation(orbNumber)
+         
+            anime({
+                targets:orbClone,
+                loop:true,
+                easing:'linear',
+                duration:5000,
+                rotate: [-orbNumber*90,-360-orbNumber*90],
+                transformOrigin:[`${cx}px ${cy}px 0`, `${cx}px ${cy}px 0`]
+            })
+            
+        
+        })
+    } 
+
+    for(let i = 0; i < orbs.length; i++){
+        animateOrb(orbs[i], orbClones[i], i, locationValues[i])
+    }
+}
+
+let animateCircle = (stats) => {
+    let orbs = document.getElementsByClassName('orbs')
+    let orbClones = document.getElementsByClassName('orbs-clones')
+    let circle = document.querySelector('.circle-container #circle')
+
+    anime({
+        targets:circle,
+        opacity:[0,1],
+        duration:3000,
+        easing:'linear',
+    })
+
+    let circleRotation = anime({
+        targets:circle,
+        rotate:360,
+        loop:true,
+        duration:5000,
+        easing:'linear',
+        update: (stats) => {
+            if(Math.round(stats.progress) === 77){
+                orbsAnimation(orbs, orbClones)
+                circleRotation.update = () => {}
+            }
+        }
+    })
+    
+  
+}
+
+export default animateCircle
